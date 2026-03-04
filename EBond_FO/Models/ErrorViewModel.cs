28782ddc67012b168073a052bbf856a603cbe9e6
Linh Phùng <phungduclinh.99@gmail.com>
2026-03-04 14:05:44 +0700

    init

---
+++ EBond_FO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EBond_FO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
