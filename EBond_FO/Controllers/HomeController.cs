1c3d79fff50562b5a1492843b59f3b8404f0fe32
Linh Phùng <phungduclinh.99@gmail.com>
2026-03-04 14:13:18 +0700

    init

---
+++ EBond_FO/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using EBond_FO.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EBond_FO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
